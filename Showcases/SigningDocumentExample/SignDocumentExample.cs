c736f0f790a6dba4837ac968bcdf51df58ee5a28
vyacheslav.deryushev <falleretic@gmail.com>
2017-12-04 10:34:17 +0700

    simplified example

--- Showcases/SigningDocumentExample/SignDocumentExample.cs
+++ Showcases/SigningDocumentExample/SignDocumentExample.cs
@@ -25,54 +25,58 @@ namespace SigningDocumentExample
         static readonly string TestImage = DataDir + @"Images\LogoSmall.png";
 
         [Test]
-        public static void Main(string[] args)
+        public static void Main()
         {
             // We need to create simple List with test signers for this example.
             CreateTestData();
-            
+            // Let's define sign person who must sign the document.
+            string signer = "SignPerson 1";
+
             // Get document that we need to sign.
             Document baseDocument = new Document(DataDir + "TestFile.docx");
             DocumentBuilder builder = new DocumentBuilder(baseDocument);
 
-            // Let's define sign person who must sign document.
-            string signer = "SignPerson 1";
+            // Get sign person object by name of the person who must sign document.
+            // This an example.
+            // Actually, you need to return object from a data base.
+            SignPerson signPersonInfo = (from c in mSignPersonList where c.Name == signer select c).FirstOrDefault();
             
             // Create holder of certificate instanse base on your personal certificate.
             // This is the test certificate generated for this example.
             CertificateHolder certificateHolder = CertificateHolder.Create(DataDir + "morzal.pfx", "aw");
 
             // Let's add signature to the document and sign it with personal certificate.
-            Document signedDocument = SignDocument(signer, builder, certificateHolder);
+            Document signedDocument = SignDocument(builder, certificateHolder, signPersonInfo);
 
             // Now we need add signed document to simple List.
-            WriteSignedDocument(signedDocument);
+            WriteSignedDocument(signedDocument, "SignedDocument");
         }
 
         /// <summary>
         /// Add signature line to the document and sign it with personal certificate
         /// </summary>
-        /// <param name="signer">Name of the person who sing document</param>
         /// <param name="builder">Class that provides methods for create SignatureLine</param>
         /// <param name="certificateHolder">Holder of personal certificate instanse</param>
+        /// <param name="signPersonInfo">SignPerson object which contains info about person who must sign document</param>
         /// <returns>Returns signed document</returns>
-        private static Document SignDocument(string signer, DocumentBuilder builder, CertificateHolder certificateHolder)
+        private static Document SignDocument(DocumentBuilder builder, CertificateHolder certificateHolder, SignPerson signPersonInfo)
         {
             // Add info about responsible person who sign document.
             SignatureLineOptions signatureLineOptions = new SignatureLineOptions();
-            signatureLineOptions.Signer = GetSignPersonByName(signer).Name;
-            signatureLineOptions.SignerTitle = GetSignPersonByName(signer).Position;
+            signatureLineOptions.Signer = signPersonInfo.Name;
+            signatureLineOptions.SignerTitle = signPersonInfo.Position;
 
             // Add signature line for responsible person who sign document.
             SignatureLine signatureLine = builder.InsertSignatureLine(signatureLineOptions).SignatureLine;
-            signatureLine.Id = GetSignPersonByName(signer).PersonId;
+            signatureLine.Id = signPersonInfo.PersonId;
 
             // Save document with line signatures into temporary file for future signing.
             builder.Document.Save(DataDir + "signedDocument.docx");
 
             // Link our signature line with personal signature.
             SignOptions signOptions = new SignOptions();
-            signOptions.SignatureLineId = GetSignPersonByName(signer).PersonId;
-            signOptions.SignatureLineImage = GetSignPersonByName(signer).Image;
+            signOptions.SignatureLineId = signPersonInfo.PersonId;
+            signOptions.SignatureLineImage = signPersonInfo.Image;
 
             // Sign our document.
             DigitalSignatureUtil.Sign(DataDir + "signedDocument.docx", DataDir + "signedDocument.docx", certificateHolder, signOptions);
@@ -84,30 +88,20 @@ namespace SigningDocumentExample
         /// Example method for add signed document to simple List
         /// </summary>
         /// <param name="signedDocument">Signed document that we need to add</param>
-        private static void WriteSignedDocument(Document signedDocument)
+        /// <param name="documentName">Name of the signed document</param>
+        private static void WriteSignedDocument(Document signedDocument, string documentName)
         {
             // This just an example.
-            // Actually, it will save or update object with data base.
+            // Actually, it will save or update object to data base.
             mSignDocumentList = new List<SignDocument>
             {
                 new SignDocument
                 {
                     DocumentId = Guid.NewGuid(),
-                    DocumentName = "SignedDocument",
+                    DocumentName = documentName,
                     Document = ConvertHepler.ConvertDocumentToByteArray(signedDocument)
                 }
             };
-        }
-
-        /// <summary>
-        /// Get sign person object from our simple list
-        /// </summary>
-        /// <param name="name">Sign person name</param>
-        private static SignPerson GetSignPersonByName(string name)
-        {
-            // This an example.
-            // Actually, it will return object from a data base.
-            return new Repository<SignPerson>(mSignPersonList.AsQueryable()).FindElement(p => p.Name == name);
         }
 
         /// <summary>
